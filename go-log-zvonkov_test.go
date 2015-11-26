a94c9da8f5b4b615a6b8392060ccb5898dbc5d98
Ilnur Saifutdinov <ilnursoft@gmail.com>
2015-11-26 17:40:14 +0300

    add func TestFilternumtelst

--- go-log-zvonkov_test.go
+++ go-log-zvonkov_test.go
@@ -3,7 +3,14 @@ package main
 
 import (
 		"testing"
+	//	"flag"
+	//	"os"
 )
+
+//func TestMain(t *testing.T) {
+//	flag.Parse()
+//	os.Exit(t.Run())
+//}
 
 // тест разбивают дату YYYY-MM-DD на 2 части: (YYYY-MM,DD)
 func TestParse_date(t *testing.T) { 
@@ -37,3 +44,20 @@ func TestSec_to_hour(t *testing.T) {
 	}
 }
 
+// тест: выборка только тех даннх которые входят в массив телефонов keys
+func TestFilternumtelst (t *testing.T) { 
+	keys:=[]string{"15204","15999","10245"}
+	numtel:=[]InputDataTel{ InputDataTel{"2015-11-26","15204",10,"+71111111"},InputDataTel{"2015-11-25","15224",7,"+778548545"},
+							InputDataTel{"2015-11-26","14999",10,"+71115811"}, InputDataTel{"2015-11-02","10245",10,"+71111111"},InputDataTel{"2015-11-25","15224",7,"+778548545"}}
+	result:=[]InputDataTel{ InputDataTel{"2015-11-26","15204",10,"+71111111"}, InputDataTel{"2015-11-02","10245",10,"+71111111"}}
+	res:=filternumtels(numtel,keys)	
+	if (len(res)!=2) {
+		t.Error("Ошибка: ваш результат  ",res)
+	} else{
+		if (res[0]!=result[0]) || (res[1]!=result[1])  {
+			t.Error("Ошибка: ваш результат  ",res)
+		}
+	}
+}
+
+ 
